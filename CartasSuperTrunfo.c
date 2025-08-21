8071a67cf1bd5bf754d4e5f83962a7416eb33b90
Francisco Kauã Fernandes Carneiro <kauafernandes24@gmail.com>
2025-08-21 17:37:28 +0000

    introdução densidade populacional e pib per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,16 +9,19 @@ typedef struct {
     float area;
     float pib;
     int pontos_turistico;
+    float densidade_populacional;
+    float pib_per_capita;
 } Carta;
 
 int main () {
     int n;
     printf("Quantas cartas deseja cadastrar? ");
     scanf("%d", &n);
-    getchar(); 
+    getchar(); // Limpa o buffer do teclado
 
     Carta cartas[n];
 
+    // Cadastro das cartas
     for (int i = 0; i < n; i++) {
         printf("\n--- Carta %d ---\n", i+1);
 
@@ -46,8 +49,13 @@ int main () {
         printf("Digite o número de pontos turísticos: \n");
         scanf("%d", &cartas[i].pontos_turistico);
 
-        getchar(); 
+        cartas[i].densidade_populacional = (float)cartas[i].populacao / cartas[i].area;
+        cartas[i].pib_per_capita = (float)cartas[i].pib * 1000000000.0 / cartas[i].populacao;
 
+        getchar(); // Limpa o buffer para a próxima carta
+    }
+
+    // Impressão das cartas cadastradas
     printf("\n=== Fichas das Cartas ===\n");
     for (int i = 0; i < n; i++) {
         printf("\nCarta %d:\n", i+1);
@@ -55,9 +63,11 @@ int main () {
         printf("Código: %s\n", cartas[i].codigocard);
         printf("Cidade: %s\n", cartas[i].cidade);
         printf("População: %d\n", cartas[i].populacao);
-        printf("Área: %.2f\n", cartas[i].area);
-        printf("PIB: %.2f\n", cartas[i].pib);
+        printf("Área: %.2f km²\n", cartas[i].area);
+        printf("PIB: %.2f bilhões de reais\n", cartas[i].pib);
         printf("Número de pontos turísticos: %d\n", cartas[i].pontos_turistico);
+        printf("Densidade populacional: %.2f hab/km²\n", cartas[i].densidade_populacional);
+        printf("PIB per Capita: %.2f reais\n", cartas[i].pib_per_capita);
     }
 
     return 0;
